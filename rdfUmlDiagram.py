98d742b37d515da7d755635992e59525b57edbdd
Markus Graube <markus.graube@tu-dresden.de>
2015-02-22 19:37:49 +0100

    New functionalities * SVG as default graphics ouput with embedded http links * datatype is shown for class properties * enhanced test files

--- rdfUmlDiagram.py
+++ rdfUmlDiagram.py
@@ -39,6 +39,7 @@ Dependencies:
 """
 
 from os.path import splitext
+
 from rdflib import Dataset
 
 from umlpygraphvizdiagram import UmlPygraphVizDiagram
@@ -48,10 +49,10 @@ class RDFtoUmlDiagram():
     """
     Transform a RDF dataset to an UML diagram
     """
-    def __init__(self, output_filename='output.png'):
+
+    def __init__(self):
         self.ds = Dataset()
-        #self.d = UmlGraphVizDiagram(output_filename)
-        self.d = UmlPygraphVizDiagram(output_filename)
+        self.d = UmlPygraphVizDiagram()
 
     def load_rdf(self, filename, input_format=None):
         if input_format:
@@ -70,6 +71,7 @@ class RDFtoUmlDiagram():
             rdf_format = format_list[extension]
         else:
             rdf_format = 'turtle'
+        print("using rdf format: " + rdf_format)
         temp = self.ds.graph("file://"+filename.name)
         temp.parse(filename.name, format=rdf_format)
 
@@ -80,9 +82,7 @@ class RDFtoUmlDiagram():
     
     def start_subgraph(self, graph_name):
         self.d.start_subgraph(graph_name.strip('[<>]:_'))
-        
-    def close_subgraph(self):
-        self.d.close_subgraph()
+
 
     def add_object_node(self, object_name, classes_name, attributes):
         self.d.add_object_node(self.ds.namespace_manager.qname(object_name), classes_name, attributes)
@@ -98,132 +98,127 @@ class RDFtoUmlDiagram():
 
     def create_namespace_box(self):
         # Create Namespace box
-        self.d.add_label("Namespaces:\l")
+        label = """<
+            <table align="left" cellborder="0">
+                <tr><td align='center' colspan='2'><b>Namespaces</b></td></tr>"""
         for ns in sorted(self.ds.namespaces()):
-            self.d.add_label("%s:\t%s \l" % (ns[0], ns[1]))
+            label += "<tr><td align='left'>%s:</td><td align='left'>%s</td></tr>" % (ns[0], ns[1] )
+        label += "</table> >"
 
-    def output_dot(self):
-        self.d.write_to_file()
+        self.d.set_label(label)
 
-    def close(self):
+    def output_dot(self, filename):
+        self.d.write_to_file(filename)
+
+    def visualize(self, filename):
+        self.d.visualize(filename, self.ds.namespaces())
+
+
+    def create_diagram(self, object_nodes=True, class_nodes=False):
+        # Iterate over all graphs
+        for graph in self.ds.contexts():
+            graph_name = graph.n3()
+            if graph_name == "[<urn:x-rdflib:default>]":
+                break
+            graph = graph.skolemize()
+            if len(graph) > 0:
+                self.start_subgraph(graph_name)
+                if object_nodes:
+                    self.create_object_nodes(graph)
+                if class_nodes:
+                    self.create_class_nodes(graph)
         self.create_namespace_box()
-        self.d.close()
-
-    def visualize(self):
-        self.d.visualize()
 
 
-class RDFtoUmlObjectDiagram(RDFtoUmlDiagram):
-    """
-    Transform a RDF dataset to an UML object diagram
-    """
-
-    def create_diagram(self):
-        # Iterate over all graphs
-        for graph in self.ds.contexts():
-            graph_name = graph.n3()
-            if graph_name == "[<urn:x-rdflib:default>]":
-                break
-            graph = graph.skolemize()
-
-            query_nodes = """SELECT DISTINCT ?node
+    def create_object_nodes(self, graph):
+        # object nodes
+        query_nodes = """ PREFIX owl: <http://www.w3.org/2002/07/owl#>
+                    SELECT DISTINCT ?node
+                    WHERE {
+                        ?node a ?class.
+                        FILTER (?class not IN (rdfs:Class, owl:Class, owl:Property, owl:ObjectProperty, owl:DatatypeProperty))
+                    } ORDER BY ?node"""
+        result_nodes = graph.query(query_nodes)
+        for row_nodes in result_nodes:
+            # adding the classes to the node (can be more than one)
+            query_classes = """SELECT DISTINCT ?class
+                    WHERE {
+                        %s a ?class.
+                    } ORDER BY ?class""" % row_nodes['node'].n3()
+            result_classes = graph.query(query_classes)
+            classes = []
+            for row_classes in result_classes:
+                classes.append(self.ds.namespace_manager.qname(row_classes['class']))
+            # adding the attributes to the node
+            query_attributes = """SELECT DISTINCT ?p ?o
                         WHERE {
-                            ?node a ?class.
-                        } ORDER BY ?node"""
-            result_nodes = graph.query(query_nodes)
+                            %s ?p ?o.
+                            FILTER (isLiteral(?o))
+                        } ORDER BY ?p ?o""" % row_nodes['node'].n3()
+            result_attributes = graph.query(query_attributes)
+            attributes = []
+            for row_attributes in result_attributes:
+                attributes.append(
+                    self.ds.namespace_manager.qname(row_attributes['p']) + " = " + str(row_attributes['o']))
+            self.add_object_node(row_nodes['node'], ", ".join(classes), attributes)
 
-            if result_nodes:
-                self.start_subgraph(graph_name)
-            else:
-                print("Warning: No instances (rdf:type) defined in graph %s" % graph_name)
-                
-            for row_nodes in result_nodes:
-                # adding the classes to the node (can be more than one)
-                query_classes = """SELECT DISTINCT ?class
+        # object node connections
+        query_connections = """SELECT DISTINCT ?c1 ?c2 ?p
+                    WHERE {
+                        ?c1 ?p ?c2.
+                        FILTER (!isLiteral(?c2))
+                        FILTER (?p not IN (rdf:type, rdfs:domain, rdfs:range, rdfs:subClassOf))
+                    } ORDER BY ?c1 ?p ?c2"""
+        result_connections = graph.query(query_connections)
+        for row_connections in result_connections:
+            self.add_edge(row_connections['c1'], row_connections['c2'], row_connections['p'])
+
+
+    def create_class_nodes(self, graph):
+        # RDFS stuff
+        query_classes = """PREFIX owl: <http://www.w3.org/2002/07/owl#>
+                    SELECT DISTINCT ?class
+                    WHERE {
+                        ?class a ?c .
+                        FILTER (?c in (rdfs:Class, owl:Class))
+                    } ORDER BY ?class"""
+        result_classes = graph.query(query_classes)
+        for row_classes in result_classes:
+            query_datatype_property = """
+                PREFIX owl: <http://www.w3.org/2002/07/owl#>
+                PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+                SELECT DISTINCT ?property ?range
                         WHERE {
-                            %s a ?class.
-                        } ORDER BY ?class""" % row_nodes['node'].n3()
-                result_classes = graph.query(query_classes)
-                classes = []
-                for row_classes in result_classes:
-                    classes.append(self.ds.namespace_manager.qname(row_classes['class']))
-                # adding the attributes to the node
-                query_attributes = """SELECT DISTINCT ?p ?o
-                            WHERE {
-                                %s ?p ?o.
-                                FILTER (isLiteral(?o))
-                            } ORDER BY ?p ?o""" % row_nodes['node'].n3()
-                result_attributes = graph.query(query_attributes)
-                attributes = []
-                for row_attributes in result_attributes:
-                    attributes.append(self.ds.namespace_manager.qname(row_attributes['p']) + " = " + str(row_attributes['o']))
-                self.add_object_node(row_nodes['node'], ", ".join(classes), attributes)
-            self.close_subgraph()
-            query_connections = """SELECT DISTINCT ?c1 ?c2 ?p
-                        WHERE {
-                            ?c1 ?p ?c2.
-                            FILTER (!isLiteral(?c2))
-                            FILTER (?p != rdf:type)
-                        } ORDER BY ?c1 ?p ?c2"""
-            result_connections = graph.query(query_connections)
-            for row_connections in result_connections:
-                self.add_edge(row_connections['c1'], row_connections['c2'], row_connections['p'])
+                            ?property rdfs:domain %s;
+                                a owl:DatatypeProperty.
+                            OPTIONAL{ ?property rdfs:range ?range. }
+                        } ORDER BY ?property""" % row_classes['class'].n3()
+            result_datatype_property = graph.query(query_datatype_property)
+            attributes = []
+            for r in result_datatype_property:
+                text = self.ds.namespace_manager.qname(r['property'])
+                if r['range']:
+                    text += " = " + self.ds.namespace_manager.qname(r['range'])
+                attributes.append(text)
+            self.add_class_node(row_classes['class'], attributes)
 
+        query_object_property = """SELECT DISTINCT ?src ?dest ?property
+                    WHERE {
+                       ?property a <http://www.w3.org/2002/07/owl#ObjectProperty>;
+                            rdfs:domain ?src;
+                            rdfs:range ?dest.
+                    } ORDER BY ?src ?property ?dest"""
+        result_object_property = graph.query(query_object_property)
+        for row_object_property in result_object_property:
+            self.add_edge(row_object_property['src'], row_object_property['dest'], row_object_property['property'])
 
-
-class RDFStoUmlClassDiagram(RDFtoUmlDiagram):
-    """
-    Transform a RDF dataset with a RDFS vocabulary to an UML class diagram
-    """
-
-    def create_diagram(self):
-        # Iterate over all graphs
-        for graph in self.ds.contexts():
-            graph_name = graph.n3()
-            if graph_name == "[<urn:x-rdflib:default>]":
-                break
-            graph = graph.skolemize()
-
-            query2 = """SELECT DISTINCT ?class
-                        WHERE {
-                            {?class a <http://www.w3.org/2000/01/rdf-schema#Class>.}
-                            UNION
-                            {?class a <http://www.w3.org/2002/07/owl#Class>.}
-                        } ORDER BY ?class"""
-            result2 = graph.query(query2)
-            if result2:
-                self.start_subgraph(graph_name)
-            for row2 in result2:
-                query3 = """SELECT DISTINCT ?property
-                            WHERE {
-                                ?property rdfs:domain %s;
-                                    a <http://www.w3.org/2002/07/owl#DatatypeProperty>.
-                            } ORDER BY ?property""" % row2['class'].n3()
-                result3 = graph.query(query3)
-                attributes = []
-                for r in result3:
-                    attributes.append(self.ds.namespace_manager.qname(r['property']))
-                self.add_class_node(row2['class'], attributes)
-            self.close_subgraph()
-
-            query4 = """SELECT DISTINCT ?src ?dest ?property
-                        WHERE {
-                           ?property a <http://www.w3.org/2002/07/owl#ObjectProperty>;
-                                rdfs:domain ?src;
-                                rdfs:range ?dest.
-                        } ORDER BY ?src ?property ?dest"""
-            result4 = graph.query(query4)
-            for row4 in result4:
-                self.add_edge(row4['src'], row4['dest'], row4['property'])
-
-            query_subclass = """SELECT DISTINCT ?src ?dest
-                        WHERE {
-                           ?src rdfs:subClassOf ?dest.
-                        } ORDER BY ?src ?dest"""
-            result_subclass = graph.query(query_subclass)
-            for row_subclass in result_subclass:
-                self.add_subclass_edge(row_subclass['src'],row_subclass['dest'])
-
+        query_subclass = """SELECT DISTINCT ?src ?dest
+                    WHERE {
+                       ?src rdfs:subClassOf ?dest.
+                    } ORDER BY ?src ?dest"""
+        result_subclass = graph.query(query_subclass)
+        for row_subclass in result_subclass:
+            self.add_subclass_edge(row_subclass['src'], row_subclass['dest'])
 
 
 if __name__ == '__main__':
@@ -232,28 +227,40 @@ if __name__ == '__main__':
 
     # Initialize command line parser
     parser = argparse.ArgumentParser(description='Creates UML Object Diagram from RDF(S) files')
-    parser.add_argument('filename', nargs='+', type=argparse.FileType('r'), help='RDF file')
-    parser.add_argument('-o', '--output', dest='png_file', nargs=1, help='Output graphics file (default is FILENAME.png)')
+    parser.add_argument('filename', nargs='+', type=argparse.FileType('r'), help='RDF file(s)')
+    parser.add_argument('-o', '--output', dest='out', nargs=1, help='Output graphics file (default is FILENAME.svg)')
     parser.add_argument('-d', '--dot', action='store_true', help='Save DOT file')
-    parser.add_argument('-s', '--rdfs', action='store_true', help='Input files should be treated as RDFS providing a class diagram instead of an object diagram')
     parser.add_argument('-n', '--namespace', metavar=('PREFIX', 'NAMESPACE'), nargs=2, action='append', help='Additional namespaces')
+    # Eigenschaften als Klasse anzeigen default=false
+    parser.add_argument('--object-nodes', dest='showobjs', action="store_true", default=True, help='Show Objects')
+    # Objekte von Klassen anzeigen default=false
+    parser.add_argument('--class-nodes', dest='showclasses', action="store_true", default=False, help='Show Classes')
+    #
+    parser.add_argument('--ontology', dest='add_ontology', action="store_true",
+                        help='Add ontology rdfs:definedBy properties')
+    # Eingabeformat der Datei angeben, ansonsten wird es aus Dateiendung geraten.
     parser.add_argument('-i', '--input', dest='format', nargs=1, default=(None,), help='Input format (xml, n3, turtle, nt, trix, trig). When blank than it is guessed from file name extension')
     args = parser.parse_args()
 
-    if args.png_file:
-        output = args.png_file[0]
+    if args.out:
+        output = args.out[0]
     else:
-        output = args.filename[0].name + '.png'
+        output = args.filename[0].name + '.svg'
 
-    if args.rdfs:
-        d = RDFStoUmlClassDiagram(output)
-    else:
-        d = RDFtoUmlObjectDiagram(output)
+    # Klassendiagramm erzeugen
+    d = RDFtoUmlDiagram()
+
+    # Darstellungsoptionen
+    print("showing objects: " + str(args.showobjs))
+    print("showing classes: " + str(args.showclasses))
+
+    # RDF-Format einstellen
     for f in args.filename:
         d.load_rdf(f, args.format[0])
     d.add_namespaces(args.namespace)
-    d.create_diagram()
-    d.close()
+    d.create_diagram(args.showobjs, args.showclasses)
     if args.dot:
-        d.output_dot()
-    d.visualize()
+        d.output_dot(output + ".dot")
+    d.visualize(output)
+
+    print("graphic was created with output file name: " + output)
--- umlpygraphvizdiagram.py
+++ umlpygraphvizdiagram.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from pygraphviz import AGraph
+import re
 import logging
+
+from pygraphviz import AGraph
+
 
 trantab = str.maketrans("./:-#", "_____")
 
@@ -13,9 +16,9 @@ class UmlPygraphVizDiagram():
     """
     Creates a diagram similar to the class diagrams and objects diagrams from UML using pygraphviz
     """
-    def __init__(self, filename='output.png'):
+
+    def __init__(self):
         logging.basicConfig()
-        self.filename = filename
         self.graph = AGraph(directed=True, strict=False)
         self.graph.node_attr['shape']='record'
         self.graph.graph_attr['fontsize']='8'
@@ -25,50 +28,115 @@ class UmlPygraphVizDiagram():
         self.connected_nodes = set()
         self.described_nodes = set()
 
-    def add_node(self, node_id, node_label):
+    def _add_node(self, node_id, node_label):
         self.graph.add_node(node_id, label=node_label)
 
     def add_class_node(self, class_name, attributes):
         self.described_nodes.add(class_name)
-        label = "<{<b>%s</b> | %s }>" % (class_name, "<br/>".join(attributes))
+        label = "<{<b>%s</b> | %s }>" % (
+            class_name, "<br align='left'/>".join(attributes).strip() + "<br align='left'/>")
         self.subgraph.add_node(graphviz_id(class_name), label=label)
 
     def add_object_node(self, object_name, class_name, attributes):
         self.described_nodes.add(class_name)
-        label = "<{<b><u>%s (%s)</u></b>| %s }>" % (object_name, class_name, '<br/>'.join(attributes))
+        label = "<{<b><u>%s (%s)</u></b>| %s }>" % (
+            object_name, class_name, "<br align='left'/>".join(attributes) + "<br align='left'/>")
         self.subgraph.add_node(graphviz_id(object_name), label=label)
 
     def add_edge(self, src, dst, name):
         self.graph.add_edge(graphviz_id(src), graphviz_id(dst), label=name, arrowhead="open")
 
+    def add_cardinality_edge(self, src, dst, name, card):
+        self.graph.add_edge(graphviz_id(src), graphviz_id(dst), label=name, arrowhead="open", labeldistance=2.0,
+                            headlabel=card + "..." + card, labelangle=-65.0)
+
+    def add_min_cardinality_edge(self, src, dst, name, card):
+        self.graph.add_edge(graphviz_id(src), graphviz_id(dst), label=name, arrowhead="open", labeldistance=2.0,
+                            headlabel=card + "...*", labelangle=-65.0)
+
+    def add_max_cardinality_edge(self, src, dst, name, card):
+        self.graph.add_edge(graphviz_id(src), graphviz_id(dst), label=name, arrowhead="open", labeldistance=2.0,
+                            headlabel="0..." + card, labelangle=-65.0)
+
+    def add_min_max_cardinality_edge(self, src, dst, name, min, max):
+        self.graph.add_edge(graphviz_id(src), graphviz_id(dst), label=name, arrowhead="open", labeldistance=2.0,
+                            headlabel=min + "..." + max, labelangle=-65.0)
+
+    def add_list_edge(self, src, dst, name):
+        self.graph.add_edge(graphviz_id(src), graphviz_id(dst), label=name, arrowtail="ediamond", dir="back",
+                            headlabel="0...*", labeldistance=2.0, labelfontcolor="black", labelangle=-65.0)
+
+    # self.graph.add_edge(graphviz_id(src), graphviz_id(dst),label=name,arrowtail="ediamond", dir="back",headlabel="0...*", taillabel=(dst.split(":")[1])+"s",labeldistance=2.0,labelfontcolor="black")
+
+    def add_symmetric_edge(self, src, dst, name):
+        self.graph.add_edge(graphviz_id(src), graphviz_id(dst), label=name, arrowhead="none")
+
+    def add_functional_edge(self, src, dst, name):
+        self.graph.add_edge(graphviz_id(src), graphviz_id(dst), label=name, arrowhead="open", headlabel="1...1",
+                            taillabel="0...*", labeldistance=2.0, labelfontcolor="black", labelangle=-65.0)
+
+    def add_inversefunctional_edge(self, src, dst, name):
+        self.graph.add_edge(graphviz_id(src), graphviz_id(dst), label=name, arrowhead="open", headlabel="0...*",
+                            taillabel="1...1", labeldistance=2.0, labelfontcolor="black", labelangle=-65.0)
+
+    def add_equivalentclass_edge(self, src, dst):
+        self.graph.add_edge(graphviz_id(src), graphviz_id(dst), label="\<\<equivalentClass\>\>", arrowhead="none",
+                            style="dashed")
+
+    def add_unionof_edge(self, src, dst):
+        self.graph.add_edge(graphviz_id(src), graphviz_id(dst), label="\<\<unionOf\>\>", arrowhead="open",
+                            style="dashed")
+
+    def add_oneof_edge(self, src, dst):
+        self.graph.add_edge(graphviz_id(src), graphviz_id(dst), label="\<\<instanceOf\>\>", arrowhead="open",
+                            style="dashed", dir="back")
+
     def add_subclass_edge(self, src, dst):
         self.graph.add_edge(graphviz_id(src), graphviz_id(dst), arrowhead="empty")
 
-    def add_label(self, label):
-        self.graph.graph_attr['label'] += label
+
+    def set_label(self, label):
+        self.graph.graph_attr['label'] = label
 
     def start_subgraph(self, graph_name):
         self.subgraph = self.graph.add_subgraph(name="cluster_%s" % graphviz_id(graph_name))
         self.subgraph.graph_attr['label']=graph_name
-
-    def close_subgraph(self):
-        pass
-
-    def close(self):
-        pass
 
     def add_undescribed_nodes(self):
         s = self.connected_nodes-self.described_nodes
         for node in s:
             self.graph.add_node(graphviz_id(node), node)
 
-    def write_to_file(self):
-        filename_dot = self.filename + '.dot'
+    def write_to_file(self, filename_dot):
         f = open(filename_dot, 'w')
         f.write(self.graph.string())
-        print("png created: " + filename_dot)
+        print("dot file created: " + filename_dot)
 
-    def visualize(self):
+    def visualize(self, filename, namespaceList=None):
         self.graph.layout(prog='dot')
-        self.graph.draw(self.filename)
-        print("png created: " + self.filename)
+        self.graph.draw(filename)
+        if filename.endswith(".svg"):
+            self._add_links_to_svg_file(filename, namespaceList)
+        print("graphic file created: " + filename)
+
+
+    def _add_links_to_svg_file(self, output, namespaceList=None):
+        # SVG Datei anpassen
+        svg_string = open(output).read()
+
+        # Titel der SVG Datei anpassen
+        svg_string = svg_string.replace("%3", output)
+
+        # Hyperlinks mit den Internetseiten hinzufügen
+        for ns in namespaceList:
+            namespace = str(ns[0])  # Präfix des Namespaces
+            url = str(ns[1])  # URL des Namespaces
+
+            regex_str = """%s:(\w+)""" % namespace
+            regex = re.compile(regex_str)
+            svg_string = regex.sub("""<a xlink:href='%s\\1'>%s:\\1</a>""" % (url, namespace), svg_string)
+
+        # Datei schreiben
+        svg_file = open(output, "w")
+        svg_file.write(svg_string)
+        svg_file.close()
